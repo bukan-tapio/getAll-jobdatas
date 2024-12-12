a8bbffba922e510c87c93182977cf4dd52d9568c
bukan-tapio <pio.tapio90@gmail.com>
2024-12-12 19:36:37 +0700

    add all

--- server.js
+++ server.js
@@ -50,7 +50,7 @@ app.get("/homePage", async (req, res) => {
   }
 });
 
-const port = process.env.PORT || 8080;
+const port = parseInt(process.env.PORT || 8080);
 app.listen(port, "0.0.0.0", () => {
   console.log(`Server running on port: ${port}`);
 });
